558c97b812a8dada61e26a291febf80cb9831da2
Striver98 <2021302191843@whu.edu.cn>
2024-10-21 20:39:10 +0800

    Initial commit

---
+++ app/src/main/java/com/example/noting/EditActivity.java
@@ -0,0 +1,43 @@
+package com.example.noting;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class EditActivity extends AppCompatActivity {
+    //找到控件
+    EditText et_1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_edit);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        et_1=findViewById(R.id.et_1);
+    }
+    //按返回键后，传回编辑结果
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+        if(keyCode==KeyEvent.KEYCODE_HOME){
+            return true;
+        } else if (keyCode==KeyEvent.KEYCODE_BACK) {
+            Intent intent= new Intent();
+            intent.putExtra("input",et_1.getText().toString());
+            setResult(RESULT_OK,intent);
+            finish();
+            return true;
+        }
+        return super.onKeyDown(keyCode,event);
+    }
+}
---
+++ app/src/main/java/com/example/noting/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.noting;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+    //声明控件
+    private FloatingActionButton btn_1;
+    final String TAG = "tag";
+    TextView tv_1;
+    ListView lv_1;
+
+    //必须
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        //找到控件
+        btn_1 = findViewById(R.id.fab_1);
+        tv_1 = findViewById(R.id.tv_1);
+        //按加号按钮实现界面跳转，编辑笔记
+        btn_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, EditActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    //接收startActivityForResult的结果
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        String edit = data.getStringExtra("input");
+        Log.d(TAG, edit);
+        tv_1.setText(edit);
+    }
+}
